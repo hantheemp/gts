8b1c98fa6a16d9c0b0a754d7b61439d93a0de09e
Murat Kağan <98017089+hantheemp@users.noreply.github.com>
2025-10-20 23:05:38 +0300

    init

---
+++ Components/Navbar/data.ts
@@ -0,0 +1,29 @@
+export const utilityLinks = [
+  {
+    name: "Home",
+    url: "/",
+  },
+  {
+    name: "Leaderboards",
+    url: "/leaderboards",
+  },
+  {
+    name: "Submit a Song!",
+    url: "https://www.youtube.com/watch?v=G0STVKbQEaQ&list=RDG0STVKbQEaQ&start_radio=1",
+  },
+];
+
+export const gameModes = [
+  {
+    name: "Daily Challenge",
+    url: "",
+  },
+  {
+    name: "Endless Mode",
+    url: "",
+  },
+  {
+    name: "Challenge a Friend",
+    url: "",
+  },
+];
