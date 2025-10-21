dbb7bf2a9433d1098c65b7c4017f624aa4fd7a12
Murat Kağan <98017089+hantheemp@users.noreply.github.com>
2025-10-21 22:03:06 +0300

    fixed font

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
 "use client";
 
-import Countdown from "@/Components/Countdown/Countdown";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-12">
-      <Countdown />
-    </main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-12"></main>
   );
 }
